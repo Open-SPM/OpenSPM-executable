d5df02c490006848e113c8d564139c7fdbbeb9a5
YourUserName <youremail@example.com>
2024-11-05 10:02:53 +0100

    OpenSPMv2.7.12: correct .bat file to not copy the original .bat files, correct problem on Scope.vi that was not resizing the graph, recompiile to correct problems with approach mechanism with QDLBNI motor

--- config/Update2latestVersionGitHub.bat
+++ config/Update2latestVersionGitHub.bat
@@ -28,7 +28,7 @@ REM Clear the token for security
 set GIT_TOKEN=
 
 REM Copy back the config and UserConfig folders from tempconfig to the repository root
-robocopy "%TEMP_DIR%\config" "config" /s /xf "Menu.rtm"
+robocopy "%TEMP_DIR%\config" "config" /s /xf "Menu.rtm" "InstallOpenSPM.bat" "Update2latestVersionGitHub.bat"
 xcopy "%TEMP_DIR%\UserConfig\." "UserConfig" /s /y
 
 REM Remove the tempconfig directory
