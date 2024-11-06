5012dd8644573d02b465def8f874d257e6ab8197
YourUserName <youremail@example.com>
2024-11-06 07:25:18 +0100

    OpenSPMv2.7.13: correct .bat file to stop asking if file or folder, correct manual movement of the tip to show the exact place on the monitor.vi for afm and sem, feedback by default off after ramping

--- config/Update2latestVersionGitHub.bat
+++ config/Update2latestVersionGitHub.bat
@@ -4,13 +4,13 @@ cd ..
 
 REM Create the tempconfig folder if it doesn't exist
 set TEMP_DIR=..\tempconfig
-if not exist "%TEMP_DIR\.%" (
+if not exist "%TEMP_DIR\NUL%" (
     mkdir "%TEMP_DIR%"
 )
 
 REM Copy the config and UserConfig folders to tempconfig
-xcopy "config" "%TEMP_DIR%\config\." /s /y
-xcopy "UserConfig" "%TEMP_DIR%\UserConfig\." /s /y
+xcopy "config" "%TEMP_DIR%\config" /s /y /i
+xcopy "UserConfig" "%TEMP_DIR%\UserConfig" /s /y /i
 
 REM Set up Git user credentials
 git config user.name "YourUserName"
@@ -29,7 +29,7 @@ set GIT_TOKEN=
 
 REM Copy back the config and UserConfig folders from tempconfig to the repository root
 robocopy "%TEMP_DIR%\config" "config" /s /xf "Menu.rtm" "InstallOpenSPM.bat" "Update2latestVersionGitHub.bat"
-xcopy "%TEMP_DIR%\UserConfig\." "UserConfig" /s /y
+xcopy "%TEMP_DIR%\UserConfig" "UserConfig" /s /y /i
 
 REM Remove the tempconfig directory
 rmdir /s /q "%TEMP_DIR%"
